84c99dc91c1124ef86c967df38e18226138a3f1e
sharee123 <shareenair15@gmail.com>
2018-08-30 18:12:54 +0530

    edit & delete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+	before_action :find_task,only:[:show, :edit, :update,:destroy]
+	def index
+		@task = Task.all
+	end
+	def new
+		@task = Task.new
+	end
+	def create
+		@task = Task.new(task_params)
+		if @task.save
+			redirect_to @task
+		else
+			render "new"
+		end
+	end
+	def show
+	end
+	def edit
+	end
+	def update
+     if @task.update(task_params)
+     	redirect_to @task
+     else
+     	render "edit"
+	end
+end
+	def destroy
+		@task.destroy
+		redirect_to root_path
+	end
+	def task_params
+		params.require(:task).permit(:title, :description, :company, :url)
+	end
+	def find_task
+		@task = Task.find(params[:id])
+	end
+end
